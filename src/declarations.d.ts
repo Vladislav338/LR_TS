7f78376fb02d84a1ea587afeda896b834dd78978
Vladislav <vladresonov@gmail.com>
2026-03-14 15:39:19 +0300

    Fix v2

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
